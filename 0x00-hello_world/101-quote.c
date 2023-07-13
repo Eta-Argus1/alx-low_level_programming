fd8538ac12e38317b57be95c156f74fdaf990984
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-12 22:50:50 -0700

    as i said cisfun but not here

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - has the code
+* Return: to check if s or f
+*/
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
