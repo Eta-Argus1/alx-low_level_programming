801ecbc01e1c6af0b7c8dd41c970715e7d9d0941
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-14 10:24:07 -0700

    message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25; i >= 0; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar('\n');
+	return (0);
+}
