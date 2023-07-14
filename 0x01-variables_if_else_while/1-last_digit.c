f9ec4af40247bfa465cdd77d453c839014fd89b1
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-14 08:30:36 -0700

    as the last one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", x, last);
 	}
-	elif(last < 6 && last != 0)
+	else if (last < 6 && last != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", x, last);
 	}
