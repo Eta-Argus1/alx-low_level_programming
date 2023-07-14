e35a439a9a9cd0344f900a6e79b5f3994701bb5c
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-14 11:02:24 -0700

    message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,15 @@ int main(void)
 	{
 		for (j = 49; j < 58; j++)
 		{
-			putchar(i);
-			putchar(j);
-			if (i != 56 || j != 57)
+			for (i > j)
 			{
-			putchar(',');
-			putchar(' ');
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
 			}
 		}
 	}
