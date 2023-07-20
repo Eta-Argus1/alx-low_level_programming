a934a045c18071a64e419c205ec709b3163abaf9
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-20 05:02:42 -0700

    message

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 	printf("%d", i);
 	for (i = 2; i <= 100; i++)
 	{
-		if (((i % 5) == 0) && ((i % 3) == 0))
+		if ((i % 3 == 0) && (i % 5 == 0))
 			printf(" FizzBuzz");
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 			printf(" Fizz");
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 			printf(" Buzz");
 		else
 			printf(" %d ", i);
