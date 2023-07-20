c7f1e53db357745f285e7ac9b4bab1e311111d9b
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-20 04:36:59 -0700

    commit message

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,12 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size == 0)
-		_putchar('\n');
-	else
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-				_putchar(35);
-			if(i != size - 1)
-				_putchar('\n');
-		}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar(35);
+		if(i != size - 1)
+			_putchar('\n');
+	}
 	_putchar('\n');
 }
