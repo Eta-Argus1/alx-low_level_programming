60b3d1d83afadcaeaaa7a75c763d7ac82e05611f
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-20 04:32:59 -0700

    commit message

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,13 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n == 0)
-		_putchar('\n');
-	else
-		for (i = 1; i <= n; i++)
-		{
-			for (j = 1; j < i; j++)
-				_putchar(' ');
-			_putchar(92);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar(92);
+		if (i < (n - 1))
 			_putchar('\n');
-		}
+	}
+	_putchar('\n');
 }
