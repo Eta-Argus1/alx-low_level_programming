586337d66050dc4f00ee173d4e914e31b12ce648
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-07-24 03:31:27 -0700

    message

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _puts -  prints a string, followed by a new line
  * @str: The string
@@ -6,9 +7,6 @@
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
+	puts(str);
 	_putchar('\n');
 }
