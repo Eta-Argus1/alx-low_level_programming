3e0c2e2be9ed420a6d2009a77fd7e5765e1ed94b
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-02 07:04:31 -0700

    message

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,10 +8,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int len = strlen(s);
-	int i;
-
-	for (i = len; i >= 0; i--)
-		_putchar(s[i]);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
 
