fa9aa37f62d060bf2fa4808c2d8142bd65428381
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-02 07:08:06 -0700

    message

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: the length
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
