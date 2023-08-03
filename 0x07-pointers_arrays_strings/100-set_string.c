0830e369990ca6d7be9e68b51f8adb31117bcfa6
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-03 07:02:50 -0700

    message

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: the string
+ * @to: the character
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
