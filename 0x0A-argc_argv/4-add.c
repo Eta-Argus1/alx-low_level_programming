501f26db2b414daed0778b1d4450ee1fdcd77a25
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-04 10:15:27 -0700

    message

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+int check(char *s);
 /**
  * main - program that adds positive numbers.
  * @argc: array length
@@ -17,19 +19,37 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (isdigit(*argv[i]))
+		if (check(argv[i]))
 		{
 			x = atoi(argv[i]);
 			result += x;
-			i++;
 		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+		i++;
 	}
 	printf("%d\n", result);
 
 	return (0);
+}
+/**
+ * check - check for digits
+ * @s: the string
+ * Return: always 0
+ */
+int check(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < strlen(s))
+	{
+		if (!isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
