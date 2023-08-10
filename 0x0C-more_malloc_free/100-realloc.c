503d9dafd9a76d6069c748705c97a4183cb2e121
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-10 07:11:15 -0700

    message

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,18 +15,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr)
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (!ptr)
-		return (malloc(new_size));
-	if (new_size > old_size)
+	else if (ptr == NULL)
+		{
+			ptr = malloc(new_size);
+			return (ptr);
+		}
+	else if (new_size > old_size)
 		ptr = realloc(ptr, new_size);
 
 	ptr = realloc(ptr, new_size);
+
+	if (ptr == NULL)
+		return (NULL);
 
 	free(ptr);
 	return (ptr);
