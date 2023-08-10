6519baf6a51f15e1ce5d1b2f1b7b641e3f76cd59
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-10 06:50:44 -0700

    message

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_realloc - function that reallocates a memory block using malloc & free
+ * @ptr: the previous pointer
+ * @old_size: the old size
+ * @new_size: the new size
+ * Return: void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	ptr = malloc(old_size);
+
+	if (!ptr)
+		ptr = malloc(new_size);
+	if (new_size > old_size)
+		ptr = realloc(ptr, new_size);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	free(ptr);
+	return (ptr);
+}
