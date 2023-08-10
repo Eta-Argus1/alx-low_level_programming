20ab7038c74b4d0432b01342bf339a49eb6ad2a7
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-10 04:24:42 -0700

    message

---
+++ 0x0C-more_malloc_free/tmp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *string_nconcat - function that concatenates two strings
+ * @s1: first string
+ * @s2: seconde string
+ * @n: number of bytes to cpy from s2
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i = 0, m = 0, j = 0, k = 0;
+
+	while (s1 && s1[m])
+		m++;
+
+	while (s2 && s2[k])
+		k++;
+
+	if (n < k)
+		s = malloc(sizeof(char) * (m + n + 1));
+	else
+		s = malloc(sizeof(char) * (m + k + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < m)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < k && i < (m + n))
+		s[i++] = s2[j++];
+
+	while (n >= k && i < (m + k))
+		 s[i++] = s2[j++];
+
+	s[i] = '\0';
+	return (s);
+}
