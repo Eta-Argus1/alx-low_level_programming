77f9381805a910b3ca5351ed3443d888a4a27ece
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-14 06:54:52 -0700

    message

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,13 +22,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (l1))
+	dog->name = malloc(sizeof(char) * (l1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (l2 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
 
 	stringcpy(new_dog->name = *name);
 	new_dog->age = age;
 	stringcpy(new_dog->owner = *owner);
 
-
+	return (new_dog);
 }
 
 /**
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int stringlength(char *s);
+char *stringcpy(char *s1, char *s2);
 
 #endif
