daa459995c7f93e664e7cb342e9443d262ef53a0
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-14 03:21:50 -0700

    message

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct Dog - dog's info
+ * @name: First member
+ * @age: second member
+ * @owner: Third member
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
