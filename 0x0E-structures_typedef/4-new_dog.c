c9a3998b65a5edb1ed550ea225cc3a6f9b587d42
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-14 07:23:38 -0700

    message

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -66,7 +66,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	dog->owner = malloc(sizeof(char) * (l2 + 1));
-	if (dog->name == NULL)
+	if (dog->owner == NULL)
 	{
 		free(dog);
 		free(dog->name);
