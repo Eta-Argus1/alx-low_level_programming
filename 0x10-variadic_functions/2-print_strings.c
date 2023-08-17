50a13a9396f1daad867a4e7af51259c9349e4bdd
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-17 08:22:13 -0700

    message

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(args, char *));
+		if (va_arg(args, char *) == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(args, char *));
 
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
