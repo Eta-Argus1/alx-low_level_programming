57c33786099c825d4a4a6aed2bff33bef84643e5
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-26 11:16:02 -0700

    message

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_list -  function that prints all the elements of a list_t list
+ * @h: the lists
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t size;
+
+	size = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is my printf file for singly linked project
