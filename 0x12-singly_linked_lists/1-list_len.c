abee61da1d6d02627a90ab8d59416c739a0f19ff
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-08-26 11:20:30 -0700

    message

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t list
+ * @h: pointer of the list
+ * Return: h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		size++;
+		h = h->next;
+	}
+	return (size);
+
+}
