f251d695845d522ddba811e4728cb0085f0a01b0
Eta-Argus1 <bourkali-ayoub@outlook.com>
2023-08-30 12:48:00 +0100

    msg

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,44 +1,43 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
- *
+ * insert_nodeint_at_index - function that inserts a new node at a given position
+ * @head: pointer to list
+ * @idx: where the new node is added
+ * @n: data to insert into it
  * Return: pointer to the new node, or NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *list1;
+	listint_t *list2 = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	list1 = malloc(sizeof(listint_t));
+	if (!list1 || !head)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	list1->n = n;
+	list1->next = NULL;
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		list1->next = *head;
+		*head = list1;
+		return (list1);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	for (i = 0; list2 && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			list1->next = list2->next;
+			list2->next = list1;
+			return (list1);
 		}
 		else
-			temp = temp->next;
+			list2 = list2->next;
 	}
 
 	return (NULL);
