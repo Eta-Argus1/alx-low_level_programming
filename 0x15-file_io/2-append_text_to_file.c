125930219eda09bd83756e41de67801878c92bd9
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-09-04 05:02:53 -0700

    message

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,13 +3,14 @@
 /**
  * append_text_to_file - function that appends text at the end of a file
  * @filename: the name of the file
- * @text_contect: the NULL terminated string to add at the end of the file
+ * @text_content: the NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, l;
+	int file, wr;
+	int l = 0;
 
 	if (filename == NULL)
 		return (-1);
