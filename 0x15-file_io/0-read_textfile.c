64ce758fc876c2ff03ac365aef7dd9365f5e9c23
Eta-Argus1git <bourkali-ayoub@outlook.com>
2023-09-04 02:02:10 -0700

    message

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,8 @@
  * to the POSIX standard output
  * @filename: pointer to the file
  * @letters:  the number of letters it should read and print
- * Return: the actual number of letters it could read and print 0 if filename NULL
+ * Return: the actual number of letters it could read and print
+ * 0 if filename NULL
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
